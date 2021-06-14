197f4040e7b25f733e353a076343f1d776ac65fd
Aleksei Romanov <aleksei@varjo.com>
2021-06-14 19:49:57 +0300

    data_on

--- simpleHttpWebServer/2/index.js
+++ simpleHttpWebServer/2/index.js
@@ -1,56 +1,56 @@
 const http = require("http");
 const fs = require('fs').promises;
 
-const host = 'localhost';
 const port = 8000;
 
-// let messages = [];
-
-// fuinction addNewMessage(user, msg) {
-//     messages.push({ userName: user, message: msg });
-// // }
-
-// const requestListener = function (req, res) {
-//     if (req.url === "/getMessages")
-
-// }
-
 const requestListener = function (req, res) {
-    console.log(
-        `Request: ${req.method}, ${req.url}`
-    );
-    console.log(
-        `Request headers: ${JSON.stringify(req.headers)}`
-    );
+  console.log(
+    `Request: ${req.method}, ${req.url}`
+  );
+  console.log(
+    `Request headers: ${JSON.stringify(req.headers)}`
+  );
+
+  if (req.url === '/newMessage') {
+    let data = "";
+    req.on('data', (chunk) => { data += chunk; });
+    req.on('end', (chunk) => {
+      console.log(`Received message: ${data}`);
+      res.setHeader("Content-Type", "text/css");
+      res.writeHead(200);
+      res.end();
+    });
+
+    return;
+  }
 
 
-    let fileName;
-    let contentType;
+  let fileName;
+  let contentType;
 
-    if (req.url === "/") {
-        fileName = "index.html";
-        contentType = "text/html";
-    }
-    else if (req.url.endsWith(".css")) {
-        fileName = req.url.substr(1);
-        contentType = "text/css";
-    } else {
-        res.writeHead(500);
-        res.end("Error, unsupported");
-        return;
-    }
+  if (req.url === "/") {
+    fileName = "index.html";
+    contentType = "text/html";
+  } else if (req.url.endsWith(".css")) {
+    fileName = req.url.substr(1);
+    contentType = "text/css";
+  } else {
+    res.writeHead(500);
+    res.end("Error, unsupported");
+    return;
+  }
 
-    fs.readFile(`${__dirname}/${fileName}`)
-        .then(contents => {
-            res.setHeader("Content-Type", contentType);
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err.message);
-            return;
-        });
+  fs.readFile(`${__dirname}/${fileName}`)
+    .then(contents => {
+      res.setHeader("Content-Type", contentType);
+      res.writeHead(200);
+      res.end(contents);
+    })
+    .catch(err => {
+      res.writeHead(500);
+      res.end(err.message);
+      return;
+    });
 };
 
 const server = http.createServer(requestListener);
